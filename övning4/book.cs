1b101c48f9534d0b0090f06f5a7bea75e5e7258b
JamesGill12345 <112473466+JamesGill12345@users.noreply.github.com>
2024-03-20 12:31:15 +0100

    Add files via upload

---
+++ övning4/book.cs
@@ -0,0 +1,15 @@
+class Book
+{
+    private string name;
+    private int pages;
+    private int currentPage = 0;
+
+
+    public Book()
+    {
+        name = "bok1";
+        pages = 100;
+    }
+    
+}
+
